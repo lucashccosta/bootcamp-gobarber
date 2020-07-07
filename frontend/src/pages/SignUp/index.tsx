7c4b9381f168c1fd6421abc2701beb8bb337b0d9
Lucas Costa <lucas.hcruz.costa@gmail.com>
2020-07-07 12:26:00 -0300

    feature: utilizando lib unform para tratar formularios

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import { FiLogIn, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import logo from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-    <>
-        <Container>
-            <Content>
-                <img src={logo} alt="GoBarber" />
-                <form>
-                    <h1>Faça seu cadastro</h1>
-                    <Input name="name" icon={FiUser} placeholder="Nome" />
-                    <Input name="email" icon={FiMail} placeholder="E-mail" />
-                    <Input name="password" type="password" icon={FiLock} placeholder="Senha"/>
-                    <Button type="submit">Cadastrar</Button>
-                </form>
+const SignUp: React.FC = () => {
+    function handleSubmit(data: object): void {
+        console.log(data);
+    }
 
-                <a href="javascript:void(0)">
-                    <FiLogIn />
-                    Fazer login
-                </a>
-            </Content>
-            <Background />
-        </Container>
-    </>
-);
+    return (
+        <>
+            <Container>
+                <Content>
+                    <img src={logo} alt="GoBarber" />
+                    <Form onSubmit={handleSubmit}>
+                        <h1>Faça seu cadastro</h1>
+                        <Input name="name" icon={FiUser} placeholder="Nome" />
+                        <Input name="email" type="email" icon={FiMail} placeholder="E-mail" />
+                        <Input name="password" type="password" icon={FiLock} placeholder="Senha" />
+                        <Button type="submit">Cadastrar</Button>
+                    </Form>
+
+                    <a href="javascript:void(0)">
+                        <FiLogIn />
+                        Fazer login
+                    </a>
+                </Content>
+                <Background />
+            </Container>
+        </>
+    );
+}
 
 export default SignUp;
