14145ec9004958ae2860f76f204c6ffa01206047
Lucas Costa <lucas.hcruz.costa@gmail.com>
2020-07-07 11:55:37 -0300

    design: isolando componentes de input e button

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => (
     <>
@@ -10,9 +12,9 @@ const SignIn: React.FC = () => (
                 <img src={logo} alt="GoBarber" />
                 <form>
                     <h1>Faça seu logon</h1>
-                    <input placeholder="E-mail" />
-                    <input type="password" placeholder="Senha"/>
-                    <button type="submit">Entrar</button>
+                    <Input name="email" icon={FiMail} placeholder="E-mail" />
+                    <Input name="password" type="password" icon={FiLock} placeholder="Senha"/>
+                    <Button type="submit">Entrar</Button>
                     <a href="javascript:void(0)">Esqueci minha senha</a>
                 </form>
 
