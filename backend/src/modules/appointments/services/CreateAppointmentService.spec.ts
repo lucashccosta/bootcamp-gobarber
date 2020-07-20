b7cc07bbe0ccf6c3dd85a39f946a7101dc15744d
Lucas Costa <lucas.hcruz.costa@gmail.com>
2020-07-20 10:18:28 -0300

    feature: primeiro teste

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,21 @@
-test('sum two numbers', () => {
-    expect(1+2).toBe(3);
+import FakeAppointmentRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+// Categorização
+describe('CreateAppointment', () => {
+    it('should be able to create a new appointment', async () => {
+        const fakeAppointmentRepository = new FakeAppointmentRepository();
+        const createAppointment = new CreateAppointmentService(fakeAppointmentRepository);
+
+        const appointment = await createAppointment.execute({
+            date: new Date(),
+            provider_id: '123123123'
+        });
+
+        expect(appointment).toHaveProperty('id');
+    });
+
+    // it('should not be able to create two appointments on the same time', () => {
+        
+    // });
 });
