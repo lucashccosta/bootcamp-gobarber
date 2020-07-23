ef1e68946a04c8b8ccaba1b847d93abc61d36417
Lucas Costa <lucas.hcruz.costa@gmail.com>
2020-07-23 12:05:10 -0300

    feature: controllers para verificar disponibilidade de dias e horarios disponivis

--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProviderController from '@modules/appointments/infra/http/controllers/ProviderController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
 
 const providersRoutes = Router();
 const providerController = new ProviderController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
 
 providersRoutes.use(ensureAuthenticated); 
 
 providersRoutes.get('/', providerController.index);
+providersRoutes.get('/:provider_id/month-availability', providerMonthAvailabilityController.index);
+providersRoutes.get('/:provider_id/day-availability', providerDayAvailabilityController.index);
 
 export default providersRoutes;
